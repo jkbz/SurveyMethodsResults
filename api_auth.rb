0e3cd49be59616b7fee14cb1c7125ea5e36faaf7
J Kyle Barry <kylebarry@air-watch.com>
2016-09-26 12:12:35 -0400

    Revert ".gitignore is now working"
    This reverts commit e7d7900ef03c35690c395fca82f1866a2c6d14e7.

---
+++ api_auth.rb
@@ -0,0 +1,18 @@
+# SurveyMethods credentials
+
+def get_api_username
+	# Insert username here
+end
+
+def get_api_token
+	# Insert API token here
+end
+
+def get_survey_id
+	# Test for a single survey
+	#  @survey_list =  [Insert_survey_id] # English
+	
+	survey_list = [Insert_survey_id_list]
+
+  return survey_list
+end
